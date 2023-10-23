ed56a3d27302dfd9aa1f07b3949f3b451e61c2e5
thadeus <thadeusnyariki94@gmail.com>
2023-10-23 14:30:18 +0300

    :lipstick: feat: completed slider mini project

--- SliderProject/slider.js
+++ SliderProject/slider.js
@@ -1,1 +1,69 @@
-console.log("It works!")
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+const images = document.querySelectorAll('.image');
+const bottom = document.querySelector('.bottom');
+
+let slideNumber = 1;
+const length = images.length;
+
+// lets create our buttons
+for (let i = 0; i < length; i++) {
+  const div = document.createElement('div');
+  div.className = 'button';
+  bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll('.button');
+buttons[0].style.backgroundColor = 'white';
+
+const resetBg = () => {
+  buttons.forEach((button) => {
+    button.style.backgroundColor = 'transparent';
+  });
+};
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    resetBg();
+    slider.style.transform = `translateX(-${index * 800}px)`;
+    slideNumber = index + 1;
+    button.style.backgroundColor = 'white';
+  });
+});
+
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+  slideNumber++;
+};
+
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+  slideNumber--;
+};
+
+const getFirstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+
+const getLastSlide = () => {
+  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+  slideNumber = length;
+};
+
+const changeColor = () => {
+  resetBg();
+  buttons[slideNumber - 1].style.backgroundColor = 'white';
+};
+
+right.addEventListener('click', () => {
+  // console.log("clicked!")
+  slideNumber < length ? nextSlide() : getFirstSlide();
+  changeColor()
+});
+
+left.addEventListener('click', () => {
+  slideNumber > 1 ? prevSlide() : getLastSlide();
+  changeColor()
+});
